e7ff800667dab49711503ca29533358edaa4019d
Rico Suter <mail@rsuter.com>
2014-12-28 18:15:00 +0100

    Added demo project

---
+++ NJsonSchema.Demo/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json.Linq;
+
+namespace NJsonSchema.Demo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var schema = JsonSchema4.FromType<Person>();
+            var schemaData = schema.ToJson();
+
+            var jsonToken = JToken.Parse("...");
+            var errors = schema.Validate(jsonToken);
+
+            foreach (var error in errors)
+                Console.WriteLine(error.Path + ": " + error.Kind);
+
+            schema = JsonSchema4.FromJson(schemaData);
+        }
+    }
+
+    public class Person
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        public Sex Sex { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        public Collection<Job> Jobs { get; set; }
+
+        [Range(2, 5)]
+        public int Test { get; set; }
+    }
+
+    public class Job
+    {
+        public string Company { get; set; }
+    }
+
+    public enum Sex
+    {
+        Male, 
+        Female
+    }
+}
