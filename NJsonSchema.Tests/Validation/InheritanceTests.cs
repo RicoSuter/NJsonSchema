0a1980792ede37611648af7cc14a58c240f778c1
Rico Suter <mail@rsuter.com>
2015-01-01 22:19:28 +0100

    Improved unit tests

--- NJsonSchema.Tests/Validation/InheritanceTests.cs
+++ NJsonSchema.Tests/Validation/InheritanceTests.cs
@@ -9,7 +9,7 @@ namespace NJsonSchema.Tests.Validation
     public class InheritanceTests
     {
         [TestMethod]
-        public void When_any_of_is_correct_then_it_should_succeed()
+        public void When_is_any_of_then_it_should_succeed()
         {
             //// Arrange
             var schema = new JsonSchema4();
@@ -32,7 +32,7 @@ namespace NJsonSchema.Tests.Validation
         }
 
         [TestMethod]
-        public void When_any_of_is_incorrect_then_it_should_succeed()
+        public void When_is_not_any_of_then_it_should_fail()
         {
             //// Arrange
             var schema = new JsonSchema4();
@@ -52,6 +52,163 @@ namespace NJsonSchema.Tests.Validation
 
             //// Assert
             Assert.AreEqual(ValidationErrorKind.NotAnyOf, errors.First().Kind);
+        }
+
+        [TestMethod]
+        public void When_is_all_of_then_it_should_succeed()
+        {
+            //// Arrange
+            var schema = new JsonSchema4();
+            schema.AnyOf.Add(new JsonSchema4
+            {
+                Type = JsonObjectType.String
+            });
+            schema.AnyOf.Add(new JsonSchema4
+            {
+                Type = JsonObjectType.String
+            });
+
+            var token = new JValue("Foo");
+
+            //// Act
+            var errors = schema.Validate(token);
+
+            //// Assert
+            Assert.AreEqual(0, errors.Count);
+        }
+
+        [TestMethod]
+        public void When_is_not_all_of_then_it_should_fail()
+        {
+            //// Arrange
+            var schema = new JsonSchema4();
+            schema.AllOf.Add(new JsonSchema4
+            {
+                Type = JsonObjectType.String
+            });
+            schema.AllOf.Add(new JsonSchema4
+            {
+                Type = JsonObjectType.Integer
+            });
+
+            var token = new JValue(5);
+
+            //// Act
+            var errors = schema.Validate(token);
+
+            //// Assert
+            Assert.AreEqual(ValidationErrorKind.NotAllOf, errors.First().Kind);
+        }
+
+        [TestMethod]
+        public void When_is_one_of_then_it_should_succeed()
+        {
+            //// Arrange
+            var schema = new JsonSchema4();
+            schema.OneOf.Add(new JsonSchema4
+            {
+                Type = JsonObjectType.String
+            });
+            schema.OneOf.Add(new JsonSchema4
+            {
+                Type = JsonObjectType.Integer
+            });
+
+            var token = new JValue(10);
+
+            //// Act
+            var errors = schema.Validate(token);
+
+            //// Assert
+            Assert.AreEqual(0, errors.Count);
+        }
+
+        [TestMethod]
+        public void When_is_not_one_of_then_it_should_fail()
+        {
+            //// Arrange
+            var schema = new JsonSchema4();
+            schema.OneOf.Add(new JsonSchema4
+            {
+                Type = JsonObjectType.String
+            });
+            schema.OneOf.Add(new JsonSchema4
+            {
+                Type = JsonObjectType.Boolean
+            });
+
+            var token = new JValue(5);
+
+            //// Act
+            var errors = schema.Validate(token);
+
+            //// Assert
+            Assert.AreEqual(ValidationErrorKind.NotOneOf, errors.First().Kind);
+        }
+
+        [TestMethod]
+        public void When_one_of_matches_multiple_then_it_should_fail()
+        {
+            //// Arrange
+            var schema = new JsonSchema4();
+            schema.OneOf.Add(new JsonSchema4
+            {
+                Type = JsonObjectType.String
+            });
+            schema.OneOf.Add(new JsonSchema4
+            {
+                Type = JsonObjectType.Integer
+            });
+            schema.OneOf.Add(new JsonSchema4
+            {
+                Type = JsonObjectType.Integer
+            });
+
+            var token = new JValue(5);
+
+            //// Act
+            var errors = schema.Validate(token);
+
+            //// Assert
+            Assert.AreEqual(ValidationErrorKind.NotOneOf, errors.First().Kind);
+        }
+
+        [TestMethod]
+        public void When_matches_excluded_schema_then_it_should_fail()
+        {
+            //// Arrange
+            var schema = new JsonSchema4();
+            schema.Not = new JsonSchema4
+            {
+                Type = JsonObjectType.String
+            };
+
+            var token = new JValue("Foo");
+
+            //// Act
+            var errors = schema.Validate(token);
+
+            //// Assert
+            Assert.AreEqual(ValidationErrorKind.ExcludedSchemaValidates, errors.First().Kind);
+        }
+
+        [TestMethod]
+        public void When_not_matches_excluded_schema_then_it_should_succeed()
+        {
+            //// Arrange
+            var schema = new JsonSchema4();
+            schema.Not = new JsonSchema4
+            {
+                Type = JsonObjectType.String
+            };
+
+            var token = new JValue(5);
+
+            //// Act
+            var errors = schema.Validate(token);
+
+            //// Assert
+            Assert.AreEqual(0, errors.Count);
         }
     }
 }
