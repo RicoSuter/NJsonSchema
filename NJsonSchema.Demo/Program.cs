d929dd2020ed6f3c028695a2deff6fcaad3d6c3e
Rico Suter <mail@rsuter.com>
2015-01-13 08:18:13 +0100

    Added JSON schema test suite

--- NJsonSchema.Demo/Program.cs
+++ NJsonSchema.Demo/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace NJsonSchema.Demo
@@ -9,18 +12,71 @@ namespace NJsonSchema.Demo
     {
         static void Main(string[] args)
         {
-            var schema = JsonSchema4.FromType<Person>();
-            var schemaData = schema.ToJson();
+            Console.BufferHeight = 2000;
+            var passes = 0;
+            var fails = 0;
+            var exceptions = 0;
+            var files = Directory.GetFiles("Tests");
+            foreach (var file in files)
+            {
+                Console.WriteLine("File: " + file);
+                var data = JArray.Parse(File.ReadAllText(file));
+                foreach (var suite in data.OfType<JObject>())
+                {
+                    var description = suite["description"].Value<string>();
+                    Console.WriteLine("  Suite: " + description);
 
-            var jsonToken = JToken.Parse("{}");
-            var errors = schema.Validate(jsonToken);
 
-            foreach (var error in errors)
-                Console.WriteLine(error.Path + ": " + error.Kind);
+                    foreach (var test in suite["tests"].OfType<JObject>())
+                    {
+                        var testDescription = test["description"].Value<string>();
+                        var valid = test["valid"].Value<bool>();
 
-            schema = JsonSchema4.FromJson(schemaData);
+                        Console.WriteLine("    Test: " + testDescription);
+                        Console.WriteLine("      Valid: " + valid);
+
+                        foreach (var value in test["data"])
+                        {
+                            try
+                            {
+                                var schema = JsonSchema4.FromJson(suite["schema"].ToString());
+                                var errors = schema.Validate(value);
+                                var success = (valid ? errors.Count == 0 : errors.Count > 0);
+                                Console.WriteLine("      Result: " + success);
+
+                                if (!success)
+                                    fails++;
+                                else
+                                    passes++;
+                            }
+                            catch (Exception ex)
+                            {
+                                exceptions++;
+                                Console.WriteLine("      Exception");// + ex.Message);
+                            }
+                        }
+                    }
+
+                }
+            }
+            Console.WriteLine("Passes: " + passes);
+            Console.WriteLine("Fails: " + fails);
+            Console.WriteLine("Exceptions: " + exceptions);
 
             Console.ReadLine();
+
+            //var schema = JsonSchema4.FromType<Person>();
+            //var schemaData = schema.ToJson();
+
+            //var jsonToken = JToken.Parse("{}");
+            //var errors = schema.Validate(jsonToken);
+
+            //foreach (var error in errors)
+            //    Console.WriteLine(error.Path + ": " + error.Kind);
+
+            //schema = JsonSchema4.FromJson(schemaData);
+
+            //Console.ReadLine();
         }
     }
 
@@ -49,7 +105,7 @@ namespace NJsonSchema.Demo
 
     public enum Sex
     {
-        Male, 
+        Male,
         Female
     }
 }
