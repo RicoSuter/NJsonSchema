fafb8a43292c40fa78c00f395300ec7f62b5fb29
Rico Suter <mail@rsuter.com>
2015-04-13 15:17:10 +0200

    Added code generation

---
+++ NJsonSchema.CodeGeneration.Tests/CSharpGeneratorTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NJsonSchema.Tests.Conversion;
+
+namespace NJsonSchema.CodeGeneration.Tests
+{
+    [TestClass]
+    public class CSharpGeneratorTests
+    {
+        [TestMethod]
+        public void When_namespace_is_set_then_it_should_appear_in_file()
+        {
+            //// Arrange
+            var schema = JsonSchema4.FromType<MyType>();
+
+            var generator = new CSharpGenerator(schema);
+            generator.Namespace = "Foobar";
+            
+            //// Act
+            var output = generator.Generate();
+            
+            //// Assert
+            Assert.IsTrue(output.Contains("namespace Foobar"));
+        }
+    }
+}
---
+++ NJsonSchema.CodeGeneration/GeneratorBase.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using Antlr4.StringTemplate;
+
+namespace NJsonSchema.CodeGeneration
+{
+    public class GeneratorBase
+    {
+        protected Template LoadTemplate(string language, string file)
+        {
+            var resourceName = string.Format("NJsonSchema.CodeGeneration.Templates.{0}.{1}.txt", language, file);
+            using (var stream = GetType().Assembly.GetManifestResourceStream(resourceName))
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    var text = reader.ReadToEnd();
+                    return new Template(text);
+                }
+            }
+        }
+    }
+}
