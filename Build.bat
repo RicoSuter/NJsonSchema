c8635b9c31f0a8bfc0f7a46713f8b9c6e64aec6b
Rico Suter <mail@rsuter.com>
2015-06-15 09:01:32 +0200

    Moved source files to src directory

--- Build.bat
+++
@@ -1,4 +0,0 @@
-"nuget/nuget" restore NJsonSchema.sln
-msbuild NJsonSchema.sln /p:Configuration=Release /t:rebuild
-"nuget/nuget" pack NJsonSchema/NJsonSchema.csproj -OutputDirectory "NuGet" -Prop Configuration=Release
-"nuget/nuget" pack NJsonSchema.CodeGeneration/NJsonSchema.CodeGeneration.csproj -OutputDirectory "NuGet" -Prop Configuration=Release
