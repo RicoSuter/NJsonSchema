8aaa4c022e586f18a11b685d2b2a2b52dfed2dc5
Rico Suter <mail@rsuter.com>
2015-08-17 20:40:03 +0200

    Updated to nuget references

--- build/Build.bat
+++ build/Build.bat
@@ -1,4 +1,4 @@
 nuget restore ../src/NJsonSchema.sln
 msbuild ../src/NJsonSchema.sln /p:Configuration=Release /t:rebuild
-nuget pack ../src/NJsonSchema/NJsonSchema.csproj -OutputDirectory "Packages" -Prop Configuration=Release
-nuget pack ../src/NJsonSchema.CodeGeneration/NJsonSchema.CodeGeneration.csproj -OutputDirectory "Packages" -Prop Configuration=Release
+nuget pack ../src/NJsonSchema/NJsonSchema.csproj -IncludeReferencedProjects -OutputDirectory "Packages" -Prop Configuration=Release
+nuget pack ../src/NJsonSchema.CodeGeneration/NJsonSchema.CodeGeneration.csproj -IncludeReferencedProjects -OutputDirectory "Packages" -Prop Configuration=Release
