fc54367479509fd49cc94073a73659fb6e5bcb3e
Rico Suter <mail@rsuter.com>
2016-04-06 15:41:33 +0200

    Support for nullable enum properties and IReadOnlyDictionary

--- src/NJsonSchema.CodeGeneration.Tests/TypeScriptObjectTests.cs
+++ src/NJsonSchema.CodeGeneration.Tests/TypeScriptObjectTests.cs
@@ -28,5 +28,25 @@ namespace NJsonSchema.CodeGeneration.Tests
             //// Assert
             Assert.IsTrue(code.Contains("Test?: any;"));
         }
+
+        public class DictionaryObjectTest
+        {
+            public IDictionary<string, object> Test { get; set; }
+        }
+
+        [TestMethod]
+        public void When_dictionary_value_is_object_then_typescript_uses_any()
+        {
+            //// Arrange
+            var schema = JsonSchema4.FromType<DictionaryObjectTest>();
+            var data = schema.ToJson();
+
+            //// Act
+            var generator = new TypeScriptGenerator(schema);
+            var code = generator.GenerateFile();
+
+            //// Assert
+            Assert.IsTrue(code.Contains("Test?: { [key: string] : any; };"));
+        }
     }
 }
