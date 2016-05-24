a6c3d5578e5db0334da658d9c7c108e4a79821b9
Jakub Konecki <jkonecki@gmail.com>
2016-05-24 13:14:09 +0100

    More HashSets.

--- src/NJsonSchema/JsonSchemaReferenceUtilities.cs
+++ src/NJsonSchema/JsonSchemaReferenceUtilities.cs
@@ -22,7 +22,7 @@ namespace NJsonSchema
         /// <param name="root">The root.</param>
         public static void UpdateSchemaReferences(object root)
         {
-            UpdateSchemaReferences(root, root, new List<object>());
+            UpdateSchemaReferences(root, root, new HashSet<object>());
         }
 
         /// <summary>Updates the <see cref="JsonSchema4.SchemaReferencePath" /> properties
@@ -31,7 +31,7 @@ namespace NJsonSchema
         /// <param name="schemaDefinitionAppender">The schema definition appender.</param>
         public static void UpdateSchemaReferencePaths(object root, ISchemaDefinitionAppender schemaDefinitionAppender)
         {
-            UpdateSchemaReferencePaths(root, root, new List<object>(), schemaDefinitionAppender);
+            UpdateSchemaReferencePaths(root, root, new HashSet<object>(), schemaDefinitionAppender);
         }
 
         /// <summary>Converts JSON references ($ref) to property references.</summary>
@@ -50,7 +50,7 @@ namespace NJsonSchema
             return data.Replace("schemaReferencePath", "$ref");
         }
 
-        private static void UpdateSchemaReferencePaths(object root, object obj, List<object> checkedObjects, ISchemaDefinitionAppender schemaDefinitionAppender)
+        private static void UpdateSchemaReferencePaths(object root, object obj, HashSet<object> checkedObjects, ISchemaDefinitionAppender schemaDefinitionAppender)
         {
             if (obj == null || obj is string)
                 return;
@@ -86,7 +86,7 @@ namespace NJsonSchema
             }
         }
 
-        private static void UpdateSchemaReferences(object root, object obj, List<object> checkedObjects)
+        private static void UpdateSchemaReferences(object root, object obj, HashSet<object> checkedObjects)
         {
             if (obj == null || obj is string)
                 return;
