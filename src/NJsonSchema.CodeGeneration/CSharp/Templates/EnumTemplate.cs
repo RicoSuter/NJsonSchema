aa01f57af77c407504d92d6dcb633393e668b500
Rico Suter <mail@rsuter.com>
2016-05-29 00:26:40 +0200

    Added documentation reading for enum in JsonSchemaGenerator, removing white spaces in XmlDoc line breaks, improved xml doc generation, avoiding name collisions in KO classes, added ClassTypes setting

--- src/NJsonSchema.CodeGeneration/CSharp/Templates/EnumTemplate.cs
+++ src/NJsonSchema.CodeGeneration/CSharp/Templates/EnumTemplate.cs
@@ -35,7 +35,7 @@ namespace NJsonSchema.CodeGeneration.CSharp.Templates
             this.Write("/// <summary>");
             
             #line 4 "C:\Data\Projects\NJsonSchema\src\NJsonSchema.CodeGeneration\CSharp\Templates\EnumTemplate.tt"
-            this.Write(this.ToStringHelper.ToStringWithCulture(Model.Description));
+            this.Write(this.ToStringHelper.ToStringWithCulture(ConversionUtilities.ConvertCSharpDocBreaks(Model.Description, 0)));
             
             #line default
             #line hidden
