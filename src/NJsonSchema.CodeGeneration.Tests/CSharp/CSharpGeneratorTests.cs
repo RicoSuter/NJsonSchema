b15225796f10f2d4388c3d8ad2a38dbca67e2109
Lauri Heiskanen <lheiskan@users.noreply.github.com>
2016-06-05 18:53:50 +0300

    Test for developing allOf support
    Contains a schema that has a an all of type with a few ref types within it.

--- src/NJsonSchema.CodeGeneration.Tests/CSharp/CSharpGeneratorTests.cs
+++ src/NJsonSchema.CodeGeneration.Tests/CSharp/CSharpGeneratorTests.cs
@@ -8,6 +8,54 @@ namespace NJsonSchema.CodeGeneration.Tests.CSharp
     [TestClass]
     public class CSharpGeneratorTests
     {
+        [TestMethod]
+        public void multiple_refs_in_all_of_should_expand_to_single_def()
+        {
+            var schema = @"{
+                '$schema': 'http://json-schema.org/draft-04/schema#',
+                'id': 'http://some.domain.com/foo.json',
+                'type': 'object',
+                'additionalProperties': false,
+                'definitions': {
+                    'tRef1': {
+                        'type': 'object',
+                        'properties': {
+                            'val1': {
+                                'type': 'string',
+                            }
+                        }
+                    },
+                    'tRef2': {
+                        'type': 'object',
+                        'properties': {
+                            'val2': {
+                                'type': 'string',
+                            }
+                        }
+                    },
+                    'tRef3': {
+                        'type': 'object',
+                        'properties': {
+                            'val3': {
+                                'type': 'string',
+                            }
+                        }
+                    }
+                },
+                'tAgg': {
+                    'allOf': [
+                        {'$ref': '#/definitions/tRef1'},
+                        {'$ref': '#/definitions/tRef2'},
+                        {'$ref': '#/definitions/tRef3'}
+                    ]
+                }
+            }";
+            var s = NJsonSchema.JsonSchema4.FromJson(schema);
+            var settings = new CSharpGeneratorSettings() { ClassStyle = CSharpClassStyle.Poco, Namespace = "ns", };
+            var gen = new CSharpGenerator(s, settings);
+            var output = gen.GenerateFile();
+            System.Console.WriteLine(output);
+        } 
 
         [TestMethod]
         public void When_property_has_boolean_default_it_is_reflected_in_the_poco()
