ebf0a77324020e13e1173a0a14a45b9160423719
Rico Suter <mail@rsuter.com>
2016-06-07 19:03:29 +0200

    v2.38

--- src/NJsonSchema/Properties/AssemblyInfo.cs
+++ src/NJsonSchema/Properties/AssemblyInfo.cs
@@ -5,4 +5,4 @@
 [assembly: AssemblyCompany("Rico Suter")]
 [assembly: AssemblyProduct("NJsonSchema")]
 [assembly: AssemblyCopyright("Copyright © Rico Suter, 2016")]
-[assembly: AssemblyVersion("2.37.*")]
+[assembly: AssemblyVersion("2.38.*")]
