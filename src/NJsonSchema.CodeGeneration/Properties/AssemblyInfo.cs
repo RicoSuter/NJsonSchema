fbf1353a39ddd2a2e0b821715f78f4344d974ab3
Rico Suter <mail@rsuter.com>
2016-06-10 10:59:14 +0200

    Added ref handling to extension code, v2.40

--- src/NJsonSchema.CodeGeneration/Properties/AssemblyInfo.cs
+++ src/NJsonSchema.CodeGeneration/Properties/AssemblyInfo.cs
@@ -6,6 +6,6 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCompany("Rico Suter")]
 [assembly: AssemblyProduct("NJsonSchema.CodeGeneration")]
 [assembly: AssemblyCopyright("Copyright © Rico Suter, 2016")]
-[assembly: AssemblyVersion("2.39.*")]
+[assembly: AssemblyVersion("2.40.*")]
 
 [assembly: InternalsVisibleTo("NJsonSchema.CodeGeneration.Tests")]
