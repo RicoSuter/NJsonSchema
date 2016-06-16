3f439e2250b3a8b9af1c77e0e4aec61c9cb8dbab
Rico Suter <mail@rsuter.com>
2016-06-16 23:59:46 +0200

    v2.45

--- src/NJsonSchema.CodeGeneration/Properties/AssemblyInfo.cs
+++ src/NJsonSchema.CodeGeneration/Properties/AssemblyInfo.cs
@@ -6,6 +6,6 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCompany("Rico Suter")]
 [assembly: AssemblyProduct("NJsonSchema.CodeGeneration")]
 [assembly: AssemblyCopyright("Copyright © Rico Suter, 2016")]
-[assembly: AssemblyVersion("2.44.*")]
+[assembly: AssemblyVersion("2.45.*")]
 
 [assembly: InternalsVisibleTo("NJsonSchema.CodeGeneration.Tests")]
