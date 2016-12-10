3d7a2b603620da5eacabc161a3133640f52a5add
Stjepan Bakrac <stjepan.bakrac@gmail.com>
2016-12-10 10:32:32 +0100

    Added unit test for line information parsing

--- src/NJsonSchema/Validation/ValidationError.cs
+++ src/NJsonSchema/Validation/ValidationError.cs
@@ -26,7 +26,8 @@ namespace NJsonSchema.Validation
             Path = propertyPath != null ? "#/" + propertyPath : "#";
 
             var lineInfo = token as IJsonLineInfo;
-            if (lineInfo != null && lineInfo.HasLineInfo())
+            _hasLineInfo = lineInfo != null && lineInfo.HasLineInfo();
+            if (HasLineInfo())
             {
                 LineNumber = lineInfo.LineNumber;
                 LinePosition = lineInfo.LinePosition;
@@ -47,10 +48,18 @@ namespace NJsonSchema.Validation
         /// <summary>Gets the property path. </summary>
         public string Path { get; private set; }
 
-        /// <summary> Gets the line number the validation failed on. </summary>
+        private readonly bool _hasLineInfo;
+
+        /// <summary>Indicates whether or not the error contains line information.</summary>
+        public bool HasLineInfo()
+        {
+            return _hasLineInfo;
+        }
+
+        /// <summary>Gets the line number the validation failed on. </summary>
         public int LineNumber { get; private set; }
 
-        /// <summary> Gets the line number the validation failed on. </summary>
+        /// <summary>Gets the line number the validation failed on. </summary>
         public int LinePosition { get; private set; }
 
         /// <summary>Returns a string that represents the current object.</summary>
