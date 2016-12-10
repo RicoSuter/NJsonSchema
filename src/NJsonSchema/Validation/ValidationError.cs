1751cd92a80d2596c84a0e24be3edfa6238d3041
Stjepan Bakrac <stjepan.bakrac@gmail.com>
2016-12-10 09:33:53 +0100

    Added line number information to errors

--- src/NJsonSchema/Validation/ValidationError.cs
+++ src/NJsonSchema/Validation/ValidationError.cs
@@ -6,6 +6,9 @@
 // <author>Rico Suter, mail@rsuter.com</author>
 //-----------------------------------------------------------------------
 
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
 namespace NJsonSchema.Validation
 {
     /// <summary>A validation error. </summary>
@@ -15,11 +18,24 @@ namespace NJsonSchema.Validation
         /// <param name="errorKind">The error kind. </param>
         /// <param name="propertyName">The property name. </param>
         /// <param name="propertyPath">The property path. </param>
-        public ValidationError(ValidationErrorKind errorKind, string propertyName, string propertyPath)
+        /// <param name="token">The token that failed to validate. </param>
+        public ValidationError(ValidationErrorKind errorKind, string propertyName, string propertyPath, JToken token)
         {
             Kind = errorKind;
             Property = propertyName;
             Path = propertyPath != null ? "#/" + propertyPath : "#";
+
+            var lineInfo = token as IJsonLineInfo;
+            if (lineInfo != null && lineInfo.HasLineInfo())
+            {
+                LineNumber = lineInfo.LineNumber;
+                LinePosition = lineInfo.LinePosition;
+            }
+            else
+            {
+                LineNumber = 0;
+                LinePosition = 0;
+            }
         }
 
         /// <summary>Gets the error kind. </summary>
@@ -30,6 +46,12 @@ namespace NJsonSchema.Validation
 
         /// <summary>Gets the property path. </summary>
         public string Path { get; private set; }
+
+        /// <summary> Gets the line number the validation failed on. </summary>
+        public int LineNumber { get; private set; }
+
+        /// <summary> Gets the line number the validation failed on. </summary>
+        public int LinePosition { get; private set; }
 
         /// <summary>Returns a string that represents the current object.</summary>
         /// <returns>A string that represents the current object.</returns>
