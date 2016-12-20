f08f0bd8b142b54907d7363437ee0c74928f89dd
Rico Suter <mail@rsuter.com>
2016-12-21 00:44:27 +0100

    Added unit test

--- src/NJsonSchema.Tests/Generation/IgnoredPropertyTests.cs
+++ src/NJsonSchema.Tests/Generation/IgnoredPropertyTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Runtime.Serialization;
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
 
@@ -15,14 +17,6 @@ namespace NJsonSchema.Tests.Generation
             public string IgnoreMe;
         }
 
-        [DataContract]
-        public class Xyz
-        {
-            [DataMember]
-            public string IncludeMe;
-
-            public string IgnoreMe;
-        }
 
         [TestMethod]
         public void When_field_has_JsonIgnoreAttribute_then_it_is_ignored()
@@ -37,6 +31,15 @@ namespace NJsonSchema.Tests.Generation
             Assert.IsFalse(json.Contains("IgnoreMe"));
         }
 
+        [DataContract]
+        public class Xyz
+        {
+            [DataMember]
+            public string IncludeMe;
+
+            public string IgnoreMe;
+        }
+
         [TestMethod]
         public void When_field_has_no_DataMemberAttribute_then_it_is_ignored()
         {
@@ -48,6 +51,32 @@ namespace NJsonSchema.Tests.Generation
 
             //// Assert
             Assert.IsFalse(json.Contains("IgnoreMe"));
+        }
+
+        public class Foo
+        {
+            public Dictionary<string, object> DynamicValues { get; set; }
+
+            [JsonIgnore]
+            public object this[string key]
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public string Bar { get; set; }
+        }
+
+        [TestMethod]
+        public void When_indexer_property_has_ignore_attribute_then_it_is_ignored()
+        {
+            //// Arrange
+            var schema = JsonSchema4.FromType<Foo>();
+
+            //// Act
+            var json = schema.ToJson();
+
+            //// Assert
+            Assert.AreEqual(2, schema.Properties.Count);
         }
     }
 }
