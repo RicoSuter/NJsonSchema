7e5f6aae5a1193fae596713da13665ab8ba81322
Rico Suter <mail@rsuter.com>
2017-02-02 10:23:05 +0100

    Fixed projects

---
+++ src/NJsonSchema.CodeGeneration.TypeScript/TypeScriptNullValue.cs
@@ -0,0 +1,20 @@
+//-----------------------------------------------------------------------
+// <copyright file="TypeScriptNullValue.cs" company="NJsonSchema">
+//     Copyright (c) Rico Suter. All rights reserved.
+// </copyright>
+// <license>https://github.com/rsuter/NJsonSchema/blob/master/LICENSE.md</license>
+// <author>Rico Suter, mail@rsuter.com</author>
+//-----------------------------------------------------------------------
+
+namespace NJsonSchema.CodeGeneration.TypeScript
+{
+    /// <summary>The TypeScript null value used in object initializers.</summary>
+    public enum TypeScriptNullValue
+    {
+        /// <summary>JavaScript null.</summary>
+        Null,
+
+        /// <summary>JavaScript undefined.</summary>
+        Undefined
+    }
+}
