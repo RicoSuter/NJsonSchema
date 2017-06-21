6eca261867f2f731b2235f5c952687aaa6d4335c
Paulo Morgado <paulo@paulomorgado.net>
2017-06-21 10:15:05 +0100

    Allow type discovery to fallback to $type

--- src/NJsonSchema/Converters/JsonInheritanceConverter.cs
+++ src/NJsonSchema/Converters/JsonInheritanceConverter.cs
@@ -111,6 +111,19 @@ namespace NJsonSchema.Converters
             var discriminator = jObject.GetValue(_discriminator).Value<string>();
             var subtype = GetObjectSubtype(objectType, discriminator);
 
+            if (subtype == null)
+            {
+                var typeInfo = jObject.GetValue("$type").Dump("$type");
+                if (typeInfo != null)
+                {
+                    subtype = Type.GetType(typeInfo.Value<string>());
+                }
+                else
+                {
+                    subtype = objectType.GetTypeInfo().Assembly.GetType(objectType.Namespace + "." + discriminator);
+                }
+            }
+
             try
             {
                 _isReading = true;
@@ -123,19 +136,11 @@ namespace NJsonSchema.Converters
         }
 
         private Type GetObjectSubtype(Type objectType, string discriminator)
-        {
-            var knownTypeAttributes = objectType.GetTypeInfo().GetCustomAttributes().Where(a => a.GetType().Name == "KnownTypeAttribute");
-            dynamic knownTypeAttribute = knownTypeAttributes.SingleOrDefault(a => IsKnownTypeTargetType(a, discriminator));
-            if (knownTypeAttribute != null)
-                return knownTypeAttribute.Type;
-
-            var typeName = objectType.Namespace + "." + discriminator;
-            return objectType.GetTypeInfo().Assembly.GetType(typeName);
-        }
-
-        private bool IsKnownTypeTargetType(dynamic attribute, string discriminator)
-        {
-            return attribute?.Type.Name == discriminator;
-        }
+            => objectType
+                .GetTypeInfo()
+                .GetCustomAttributes()
+                .OfType<KnownTypeAttribute>()
+                .SingleOrDefault(a => a.Type.Name == discriminator)
+                ?.Type;
     }
 }
