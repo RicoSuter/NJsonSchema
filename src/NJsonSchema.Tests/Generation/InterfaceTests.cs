6402dec47f4fafd73a0c978ca35b4f1bf026169a
Rico Suter <mail@rsuter.com>
2017-06-23 18:26:01 +0200

    Added GenerateAbstractProperties, closes https://github.com/NSwag/NSwag/issues/812

---
+++ src/NJsonSchema.Tests/Generation/InterfaceTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NJsonSchema.Generation;
+
+namespace NJsonSchema.Tests.Generation
+{
+    [TestClass]
+    public class InterfaceTests
+    {
+        public class BusinessCategory : ICategory
+        {
+            public string Key { get; set; }
+
+            public string DisplayName { get; set; }
+
+            public IEnumerable<BusinessCategory> Children { get; set; }
+
+            public IEnumerable<ICategory> Elements { get; set; }
+        }
+
+        public interface ICategory
+        {
+            string DisplayName { get; set; }
+
+            string Key { get; set; }
+        }
+
+        [TestMethod]
+        public async Task When_class_inherits_from_interface_then_properties_for_interface_are_generated()
+        {
+            //// Arrange
+
+            //// Act
+            var schema = await JsonSchema4.FromTypeAsync<BusinessCategory>(new JsonSchemaGeneratorSettings
+            {
+                GenerateAbstractProperties = true
+            });
+
+            var json = schema.ToJson();
+
+            //// Assert
+            Assert.AreEqual(2, schema.Definitions["ICategory"].Properties.Count);
+        }
+    }
+}
