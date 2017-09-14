bae782dc3bc5db0d43558d806c35c4dd8a02d623
Jens Theisen <pass-github@zokfotpik.de>
2017-09-14 14:00:13 +0200

    Nullability tests for TypeScript2.

---
+++ src/NJsonSchema.CodeGeneration.Tests/TypeScript/NullabilityTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NJsonSchema.CodeGeneration.Tests.Models;
+using NJsonSchema.CodeGeneration.TypeScript;
+using NJsonSchema.Generation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NJsonSchema.CodeGeneration.Tests.TypeScript
+{
+    [TestClass]
+    public class NullabilityTests
+    {
+        [TestMethod]
+        public async Task Strict_nullability_in_TypeScript2()
+        {
+            var schema = await JsonSchema4.FromTypeAsync<Person>(
+                new JsonSchemaGeneratorSettings {
+                    DefaultReferenceTypeNullHandling = ReferenceTypeNullHandling.NotNull
+                });
+
+            var generator = new TypeScriptGenerator(schema,
+                new TypeScriptGeneratorSettings { TypeScriptVersion = 2 });
+
+            var output = generator.GenerateFile("MyClass");
+
+            Assert.IsTrue(output.Contains("timeSpan: string;"));
+            Assert.IsTrue(output.Contains("gender: string;"));
+            Assert.IsTrue(output.Contains("address: string;"));
+
+            Assert.IsTrue(output.Contains("timeSpanOrNull: string | undefined;"));
+            Assert.IsTrue(output.Contains("genderOrNull: string | undefined;"));
+            Assert.IsTrue(output.Contains("addressOrNull: string | undefined;"));
+        }
+    }
+}
