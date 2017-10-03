9c6f8c9977eefc1255497b54431fe551c1b870a2
Rico Suter <mail@rsuter.com>
2017-10-03 13:20:17 +0200

    Update JsonSerializerSettings.cs

--- src/NJsonSchema/JsonSerializerSettings.cs
+++ src/NJsonSchema/JsonSerializerSettings.cs
@@ -1,20 +1,32 @@
-﻿namespace NJsonSchema.CodeGeneration.CSharp
+﻿//-----------------------------------------------------------------------
+// <copyright file="JsonSerializerSettings.cs" company="NJsonSchema">
+//     Copyright (c) Rico Suter. All rights reserved.
+// </copyright>
+// <license>https://github.com/rsuter/NJsonSchema/blob/master/LICENSE.md</license>
+// <author>Rico Suter, mail@rsuter.com</author>
+//-----------------------------------------------------------------------
+
+using Newtonsoft.Json;
+
+namespace NJsonSchema.CodeGeneration.CSharp
 {
+    /// <summary>The JSON serializer settings for the generated code.</summary>
     public class JsonSerializerSettings
     {
-        public Newtonsoft.Json.DateParseHandling? DateParseHandling { get; set; }
+        /// <summary>Gets or sets the DateParseHandling settings.</summary>
+        public DateParseHandling? DateParseHandling { get; set; }
 
+        /// <summary>Gets or sets the DateFormatString settings.</summary>
         public string DateFormatString { get; set; }
 
-        override
-        public string ToString() {
-            var settingsString = DateParseHandling == null ? 
-                                    string.Empty : 
-                                    $"DateParseHandling = Newtonsoft.Json.DateParseHandling.{DateParseHandling.ToString()}";
+        /// <summary>Returns a string that represents the current object.</summary>
+        /// <returns>A string that represents the current object.</returns>
+        public override string ToString() {
+            var settingsString = DateParseHandling == null ? string.Empty :
+                $"DateParseHandling = Newtonsoft.Json.DateParseHandling.{DateParseHandling.ToString()}, ";
             
-            settingsString += string.IsNullOrEmpty(DateFormatString) ? 
-                                    string.Empty : 
-                                    $"DateFormatString = \"{DateFormatString}\"";
+            settingsString += string.IsNullOrEmpty(DateFormatString) ? string.Empty : 
+                $"DateFormatString = \"{DateFormatString}\", ";
 
             return settingsString;
         }
