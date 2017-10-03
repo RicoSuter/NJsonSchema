7217143931d78eeb1ecb8c5e05afc6886e39eb5f
Rico Suter <mail@rsuter.com>
2017-10-03 09:40:16 +0200

    Removed unused whitespace

--- src/NJsonSchema.CodeGeneration.CSharp/CSharpTypeResolver.cs
+++ src/NJsonSchema.CodeGeneration.CSharp/CSharpTypeResolver.cs
@@ -80,7 +80,7 @@ namespace NJsonSchema.CodeGeneration.CSharp
         /// <summary>Generates all necessary classes.</summary>
         /// <returns>The code.</returns>
         public string GenerateClasses()
-        {        
+        {
             var classes = GenerateTypes(null);
 
             // TODO: Move utility classes to FileTemplate.tt
