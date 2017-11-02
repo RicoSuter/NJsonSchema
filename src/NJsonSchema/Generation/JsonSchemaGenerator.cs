9844be51fbac5ed0e4b759527f92edbcf58a7c10
alb3ric <alb3ric@users.noreply.github.com>
2017-11-02 12:36:05 +0100

    Update JsonSchemaGenerator.cs

--- src/NJsonSchema/Generation/JsonSchemaGenerator.cs
+++ src/NJsonSchema/Generation/JsonSchemaGenerator.cs
@@ -751,7 +751,7 @@ namespace NJsonSchema.Generation
                         dynamic readOnlyAttribute = propertyAttributes.TryGetIfAssignableTo("System.ComponentModel.ReadOnlyAttribute");
                         if (readOnlyAttribute != null)
                             p.IsReadOnly = readOnlyAttribute.IsReadOnly;
-                        if (p.Description != null)
+                        if (p.Description == null)
                             p.Description = await propertyInfo.GetDescriptionAsync(propertyAttributes).ConfigureAwait(false);
                         p.Default = ConvertDefaultValue(property);
 
