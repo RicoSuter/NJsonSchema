5327c6c69dc8adb5dcf61040b54c2e2879aadc86
Rico Suter <mail@rsuter.com>
2018-02-06 17:55:35 +0100

    Improved abstract class generation (TS), closes #612

--- src/NJsonSchema.CodeGeneration.TypeScript.Tests/AbstractGenerationTests.cs
+++ src/NJsonSchema.CodeGeneration.TypeScript.Tests/AbstractGenerationTests.cs
@@ -1,11 +1,15 @@
-﻿using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using NJsonSchema.Converters;
 using Xunit;
 
 namespace NJsonSchema.CodeGeneration.TypeScript.Tests
 {
     public class AbstractGenerationTests
     {
-        public abstract class AbstractClass
+        public abstract class AbstractClass : BaseClass
         {
             public string Foo { get; set; }
         }
@@ -22,6 +26,53 @@ namespace NJsonSchema.CodeGeneration.TypeScript.Tests
 
             /// Assert
             Assert.Contains("export abstract class AbstractClass", code);
+        }
+
+        public class ContainerClass
+        {
+            [Required]
+            public AbstractClass Foo { get; set; }
+        }
+
+        [Fact]
+        public async Task When_property_is_required_and_abstract_then_it_is_not_instantiated()
+        {
+            /// Arrange
+            var schema = await JsonSchema4.FromTypeAsync<ContainerClass>();
+
+            /// Act
+            var generator = new TypeScriptGenerator(schema, new TypeScriptGeneratorSettings { TypeScriptVersion = 2.0m });
+            var code = generator.GenerateFile("ContainerClass");
+
+            /// Assert
+            Assert.Contains("this.foo = data[\"Foo\"] ? AbstractClass.fromJS(data[\"Foo\"]) : <any>undefined;", code);
+        }
+
+        [KnownType(typeof(SuperClass))]
+        [KnownType(typeof(AbstractClass))]
+        [JsonConverter(typeof(JsonInheritanceConverter))]
+        public class BaseClass
+        {
+
+        }
+
+        public class SuperClass : AbstractClass
+        {
+
+        }
+
+        [Fact]
+        public async Task When_abstract_class_is_in_inheritance_hierarchy_then_it_is_newer_instantiated()
+        {
+            /// Arrange
+            var schema = await JsonSchema4.FromTypeAsync<AbstractClass>();
+
+            /// Act
+            var generator = new TypeScriptGenerator(schema, new TypeScriptGeneratorSettings { TypeScriptVersion = 2.0m });
+            var code = generator.GenerateFile("AbstractClass");
+
+            /// Assert
+            Assert.DoesNotContain("new AbstractClass();", code);
         }
     }
 }
