09454191589ed410483adf8b0b82db92b0da678b
Rico Suter <mail@rsuter.com>
2018-04-20 18:37:36 +0200

    Update ClassTemplateModel.cs

--- src/NJsonSchema.CodeGeneration.CSharp/Models/ClassTemplateModel.cs
+++ src/NJsonSchema.CodeGeneration.CSharp/Models/ClassTemplateModel.cs
@@ -96,9 +96,8 @@ namespace NJsonSchema.CodeGeneration.CSharp.Models
         /// <summary>Gets or sets the access modifier of generated classes and interfaces.</summary>
         public string TypeAccessModifier => _settings.TypeAccessModifier;
 
-        /// <summary></summary>
-        public string SetPropertyTypeAccessModifier => _settings.SetPropertyTypeAccessModifier;
-
+        /// <summary>Gets the access modifier of property setters (default: '').</summary>
+        public string SetPropertyTypeAccessModifier => !string.IsNullOrEmpty(_settings.SetPropertyTypeAccessModifier) ? _settings.SetPropertyTypeAccessModifier + " " : "";
 
         /// <summary>Gets the JSON serializer parameter code.</summary>
         public string JsonSerializerParameterCode => CSharpJsonSerializerGenerator.GenerateJsonSerializerParameterCode(_settings, null);
