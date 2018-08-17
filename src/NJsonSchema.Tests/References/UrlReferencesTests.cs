b8948a121927f882ec8259822bc18e96008792ac
Rico Suter <mail@rsuter.com>
2018-08-17 19:43:26 +0200

    Added second test

--- src/NJsonSchema.Tests/References/UrlReferencesTests.cs
+++ src/NJsonSchema.Tests/References/UrlReferencesTests.cs
@@ -5,15 +5,30 @@ namespace NJsonSchema.Tests.References
 {
     public class UrlReferencesTests
     {
+        // see https://github.com/RSuter/NJsonSchema/issues/566
+
         [Fact]
-        public async Task Test()
+        public async Task Abc()
+        {
+            // Arrange
+
+            // Act
+            var schema = await JsonSchema4.FromUrlAsync(@"https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json");
+
+            // Assert
+        }
+
+        [Fact]
+        public async Task Def()
         {
             // Arrange
 
             // see https://github.com/RSuter/NJsonSchema/issues/566
 
             // Act
-            var schema = await JsonSchema4.FromUrlAsync(@"https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json");
+            var schema = await
+                JsonSchema4.FromUrlAsync(
+                    "http://datafactories.schema.management.azure.com/schemas/2015-09-01/Microsoft.DataFactory.Pipeline.json");
 
             // Assert
         }
