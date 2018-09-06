4dc73870993ce08cec21ffb6c0e3a63e576aab23
Rico Suter <mail@rsuter.com>
2018-09-06 09:39:00 +0200

    Rename attribute property, https://github.com/RSuter/NSwag/pull/1391

--- src/NJsonSchema/Generation/JsonSchemaGenerator.cs
+++ src/NJsonSchema/Generation/JsonSchemaGenerator.cs
@@ -375,14 +375,14 @@ namespace NJsonSchema.Generation
                 // class
                 var extensionDataAttributes = type.GetTypeInfo().GetCustomAttributes<JsonSchemaExtensionDataAttribute>().ToArray();
                 if (extensionDataAttributes.Any())
-                    schema.ExtensionData = extensionDataAttributes.ToDictionary(a => a.Property, a => a.Value);
+                    schema.ExtensionData = extensionDataAttributes.ToDictionary(a => a.Key, a => a.Value);
             }
             else
             {
                 // property or parameter
                 var extensionDataAttributes = parentAttributes.OfType<JsonSchemaExtensionDataAttribute>().ToArray();
                 if (extensionDataAttributes.Any())
-                    schema.ExtensionData = extensionDataAttributes.ToDictionary(a => a.Property, a => a.Value);
+                    schema.ExtensionData = extensionDataAttributes.ToDictionary(a => a.Key, a => a.Value);
             }
         }
 
