973a8573b5a3bb1103f763ce73477b63591220a8
Emil Westholm <emilw@medius-group.com>
2018-10-04 12:47:53 +0200

    Incorrect generation for xmlObject when arrays are serialized with XML serializer.

--- src/NJsonSchema/Infrastructure/XmlObjectExtension.cs
+++ src/NJsonSchema/Infrastructure/XmlObjectExtension.cs
@@ -29,14 +29,35 @@ namespace NJsonSchema.Infrastructure
                     GenerateXmlObject(xmlTypeAttribute.TypeName, xmlTypeAttribute.Namespace, false, false, schema);
             }
         }
+
+        /// <summary>
+        /// Generate XML object for a JSON Schema definition
+        /// </summary>
+        /// <param name="schema">The JSON Schema</param>
+        /// <param name="type">The array type</param>
+        public static void GenerateXmlObjectForArrayType(this JsonSchema4 schema, Type type)
+        {
+            if (schema.IsArray && schema.ParentSchema == null)
+            {
+                GenerateXmlObject($@"ArrayOf{schema.Item.Xml.Name}", null, true, false, schema);
+            }
+        }
+
         /// <summary>Generates XMLObject structure for an array with primitive types</summary>
         /// <param name="schema">The JSON Schema of the item.</param>
         /// <param name="type">The item type.</param>
         public static void GenerateXmlObjectForItemType(this JsonSchema4 schema, Type type)
         {
             //Is done all the time for XML to be able to get type name as the element name if not there was an attribute defined since earlier
-            if (schema.Xml == null)
-                GenerateXmlObject(type.Name, null, false, false, schema);
+            var attributes = type.GetTypeInfo().GetCustomAttributes().ToList();
+            dynamic xmlTypeAttribute = attributes.TryGetIfAssignableTo("System.Xml.Serialization.XmlTypeAttribute");
+
+            var itemName = GetXmlItemName(type);
+
+            if (xmlTypeAttribute != null)
+                itemName = xmlTypeAttribute.TypeName;
+
+            GenerateXmlObject(itemName, null, false, false, schema);
         }
 
         /// <summary>Generates XMLObject structure for a property.</summary>
@@ -113,6 +134,26 @@ namespace NJsonSchema.Infrastructure
                 ParentSchema = schema,
                 Attribute = isAttribute
             };
+        }
+
+        /// <summary>
+        /// If type.Name is used int will return Int32, string will return String etc. These are not valid with how the XMLSerializer perform.
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        private static string GetXmlItemName(Type type)
+        {
+            if (type == typeof(int))
+                return "int";
+            else if (type == typeof(string))
+                return "string";
+            else if (type == typeof(double))
+                return "double";
+            else if (type == typeof(decimal))
+                return "decimal";
+            else
+                return type.Name;
+
         }
     }
 }
