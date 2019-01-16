1f30587311c003914a384994b789a4929fab0fd8
Rico Suter <mail@rsuter.com>
2019-01-16 16:04:53 +0100

    Do not change schema type for inlined arrays, closes #874

--- src/NJsonSchema.Tests/Generation/ArrayGenerationTests.cs
+++ src/NJsonSchema.Tests/Generation/ArrayGenerationTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
 using NJsonSchema.Generation;
 using Xunit;
@@ -26,6 +27,30 @@ namespace NJsonSchema.Tests.Generation
             var arrayProperty = schema.ActualProperties["Array"].ActualTypeSchema;
             Assert.Equal(JsonObjectType.Array, arrayProperty.Type);
             Assert.True(arrayProperty.Item.ActualTypeSchema.IsAnyType);
+        }
+
+        public class ListContainer
+        {
+            public SomeModelCollectionResponse Response { get; set; }
+        }
+
+        public class SomeModel
+        {
+            public string Foo { get; set; }
+        }
+
+        public class SomeModelCollectionResponse : List<SomeModel> { }
+
+        [Fact]
+        public async Task When_class_inherits_from_list_then_schema_is_inlined_and_type_is_array()
+        {
+            //// Act
+            var schema = await JsonSchema4.FromTypeAsync<ListContainer>(new JsonSchemaGeneratorSettings { SchemaType = SchemaType.OpenApi3 });
+            var json = schema.ToJson();
+
+            //// Assert
+            Assert.Equal(JsonObjectType.Array, schema.Definitions["SomeModelCollectionResponse"].Type);
+            Assert.NotNull(schema.Definitions["SomeModelCollectionResponse"].Item);
         }
     }
 }
