61e4f21639753377026b4fe82f6a7807e54fd20f
Rico Suter <mail@rsuter.com>
2019-03-19 21:43:10 +0100

    Name empty enum names as "Empty", closes https://github.com/RSuter/NSwag/issues/2011

--- src/NJsonSchema.CodeGeneration/DefaultEnumNameGenerator.cs
+++ src/NJsonSchema.CodeGeneration/DefaultEnumNameGenerator.cs
@@ -19,6 +19,11 @@ namespace NJsonSchema.CodeGeneration
         /// <returns>The enumeration name.</returns>
         public string Generate(int index, string name, object value, JsonSchema4 schema)
         {
+            if (string.IsNullOrEmpty(name))
+            {
+                return "Empty";
+            }
+
             if (name.StartsWith("_-"))
             {
                 name = "__" + name.Substring(2);
