65156d9bc3cc8c27a12db024eca84973fb99d09a
Rico Suter <mail@rsuter.com>
2019-03-20 21:43:52 +0100

    Add constructor ReservedPropertyNames in TypeScriptPropertyNameGenerator, closes https://github.com/RSuter/NSwag/issues/1997

--- src/NJsonSchema.CodeGeneration.TypeScript/TypeScriptPropertyNameGenerator.cs
+++ src/NJsonSchema.CodeGeneration.TypeScript/TypeScriptPropertyNameGenerator.cs
@@ -6,17 +6,23 @@
 // <author>Rico Suter, mail@rsuter.com</author>
 //-----------------------------------------------------------------------
 
+using System.Collections.Generic;
+using System.Linq;
+
 namespace NJsonSchema.CodeGeneration.TypeScript
 {
     /// <summary>Generates the property name for a given TypeScript <see cref="JsonProperty"/>.</summary>
     public class TypeScriptPropertyNameGenerator : IPropertyNameGenerator
     {
+        /// <summary>Gets or sets the reserved names.</summary>
+        public IEnumerable<string> ReservedPropertyNames { get; set; } = new List<string> { "constructor" };
+
         /// <summary>Generates the property name.</summary>
         /// <param name="property">The property.</param>
         /// <returns>The new name.</returns>
         public virtual string Generate(JsonProperty property)
         {
-            return ConversionUtilities.ConvertToLowerCamelCase(property.Name
+            var name = ConversionUtilities.ConvertToLowerCamelCase(property.Name
                     .Replace("\"", string.Empty)
                     .Replace("@", string.Empty)
                     .Replace(".", "-")
@@ -25,6 +31,13 @@ namespace NJsonSchema.CodeGeneration.TypeScript
                 .Replace("*", "Star")
                 .Replace(":", "_")
                 .Replace("-", "_");
+
+            if (ReservedPropertyNames.Contains(name))
+            {
+                return name + "_";
+            }
+
+            return name;
         }
     }
 }
