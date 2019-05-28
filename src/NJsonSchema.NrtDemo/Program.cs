f01a9a3e12e83eb9a404abb3333c1a4b43bdbc54
Rico Suter <mail@rsuter.com>
2019-05-28 07:33:50 +0200

    Fix NRT and change default DefaultReferenceTypeNullHandling setting to Default

---
+++ src/NJsonSchema.NrtDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NJsonSchema.NrtDemo
+{
+    class Program
+    {
+        public class Person
+        {
+            public string? FirstName { get; set; }
+
+            public string MiddleName { get; set; }
+
+            public string? LastName { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            var schema = JsonSchema.FromType<Person>();
+            var json = schema.ToJson();
+
+            Console.WriteLine(json);
+            Console.ReadKey();
+        }
+    }
+}
