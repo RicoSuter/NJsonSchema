95a26f6c1c92afdd7d97cdfd49af29a14cbf38c8
Rico Suter <mail@rsuter.com>
2019-08-20 20:49:16 +0200

    Add test

---
+++ src/NJsonSchema.CodeGeneration.CSharp.Tests/NullableTests.cs
@@ -0,0 +1,67 @@
+﻿using NJsonSchema.CodeGeneration.CSharp;
+using NJsonSchema.Generation;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NJsonSchema.CodeGeneration.Tests.CSharp
+{
+    public class NullableTests
+    {
+        private class ClassWithRequiredObject
+        {
+            public int Property { get; set; }
+
+            [Required]
+            public int Property2 { get; set; }
+        }
+
+        [Fact]
+        public async Task When_property_is_optional_and_GenerateNullableOptionalProperties_is_not_set_then_CSharp_property_is_not_nullable()
+        {
+            //// Arrange
+            var schema = JsonSchema.FromType<ClassWithRequiredObject>(new JsonSchemaGeneratorSettings
+            {
+                SchemaType = SchemaType.OpenApi3
+            });
+            var schemaData = schema.ToJson();
+
+            //// Act
+            var generator = new CSharpGenerator(schema, new CSharpGeneratorSettings
+            {
+                ClassStyle = CSharpClassStyle.Poco,
+                SchemaType = SchemaType.OpenApi3,
+                //GenerateNullableOptionalProperties = false
+            });
+            var code = generator.GenerateFile("MyClass");
+
+            //// Assert
+            Assert.Contains("public int Property { get; set; }", code);
+            Assert.Contains("public int Property2 { get; set; }", code);
+        }
+
+        [Fact]
+        public async Task When_property_is_optional_and_GenerateNullableOptionalProperties_is_set_then_CSharp_property_is_nullable()
+        {
+            //// Arrange
+            var schema = JsonSchema.FromType<ClassWithRequiredObject>(new JsonSchemaGeneratorSettings
+            {
+                SchemaType = SchemaType.OpenApi3
+            });
+            var schemaData = schema.ToJson();
+
+            //// Act
+            var generator = new CSharpGenerator(schema, new CSharpGeneratorSettings
+            {
+                ClassStyle = CSharpClassStyle.Poco,
+                SchemaType = SchemaType.OpenApi3,
+                GenerateNullableOptionalProperties = true
+            });
+            var code = generator.GenerateFile("MyClass");
+
+            //// Assert
+            Assert.Contains("public int? Property { get; set; }", code);
+            Assert.Contains("public int Property2 { get; set; }", code);
+        }
+    }
+}
