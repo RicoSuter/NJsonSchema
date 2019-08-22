6ce4274df70f5a706ea62b636c2af035a5f5183c
Rico Suter <mail@rsuter.com>
2019-08-22 18:51:21 +0200

    Code formatting

--- src/NJsonSchema.CodeGeneration.CSharp/Models/EnumTemplateModel.cs
+++ src/NJsonSchema.CodeGeneration.CSharp/Models/EnumTemplateModel.cs
@@ -57,22 +57,21 @@ namespace NJsonSchema.CodeGeneration.CSharp.Models
                 var entries = new List<EnumerationItemModel>();
                 for (var i = 0; i < _schema.Enumeration.Count; i++)
                 {
-                    object value = _schema.Enumeration.ElementAt(i);
+                    var value = _schema.Enumeration.ElementAt(i);
                     if (value != null)
                     {
-                        if( _schema.Type.HasFlag(JsonObjectType.Integer) )
+                        if (_schema.Type.HasFlag(JsonObjectType.Integer))
                         {
-                            string name = _schema.EnumerationNames.Count > i ?
+                            var name = _schema.EnumerationNames.Count > i ?
                                 _schema.EnumerationNames.ElementAt(i) : "_" + value;
 
-
-                            if( _schema.IsFlagEnumerable && TryGetInt64( value, out long valueInt64 ) )
+                            if (_schema.IsFlagEnumerable && TryGetInt64(value, out long valueInt64))
                             {
                                 entries.Add(new EnumerationItemModel
                                 {
-                                    Name              = _settings.EnumNameGenerator.Generate(i, name, value, _schema),
-                                    Value             = value.ToString(),
-                                    InternalValue     = valueInt64.ToString(),
+                                    Name = _settings.EnumNameGenerator.Generate(i, name, value, _schema),
+                                    Value = value.ToString(),
+                                    InternalValue = valueInt64.ToString(),
                                     InternalFlagValue = valueInt64.ToString()
                                 });
                             }
@@ -80,16 +79,16 @@ namespace NJsonSchema.CodeGeneration.CSharp.Models
                             {
                                 entries.Add(new EnumerationItemModel
                                 {
-                                    Name              = _settings.EnumNameGenerator.Generate(i, name, value, _schema),
-                                    Value             = value.ToString(),
-                                    InternalValue     = value.ToString(),
+                                    Name = _settings.EnumNameGenerator.Generate(i, name, value, _schema),
+                                    Value = value.ToString(),
+                                    InternalValue = value.ToString(),
                                     InternalFlagValue = (1 << i).ToString()
                                 });
                             }
                         }
                         else
                         {
-                            string name = _schema.EnumerationNames.Count > i ?
+                            var name = _schema.EnumerationNames.Count > i ?
                                 _schema.EnumerationNames.ElementAt(i) : value.ToString();
 
                             entries.Add(new EnumerationItemModel
@@ -107,54 +106,54 @@ namespace NJsonSchema.CodeGeneration.CSharp.Models
             }
         }
 
-        private static bool TryGetInt64( object value, out long valueInt64 )
+        private static bool TryGetInt64(object value, out long valueInt64)
         {
-            if( value is byte b )
+            if (value is byte b)
             {
                 valueInt64 = b;
                 return true;
             }
-            else if( value is sbyte sb )
+            else if (value is sbyte sb)
             {
                 valueInt64 = sb;
                 return true;
             }
-            else if( value is short int16 )
+            else if (value is short int16)
             {
                 valueInt64 = int16;
                 return true;
             }
-            else if( value is ushort uint16 )
+            else if (value is ushort uint16)
             {
                 valueInt64 = uint16;
                 return true;
             }
-            else if( value is int int32 )
+            else if (value is int int32)
             {
                 valueInt64 = int32;
                 return true;
             }
-            else if( value is uint uint32 )
+            else if (value is uint uint32)
             {
                 valueInt64 = uint32;
                 return true;
             }
-            else if( value is long int64 )
+            else if (value is long int64)
             {
                 valueInt64 = int64;
                 return true;
             }
-            else if( value is ulong uint64 )
+            else if (value is ulong uint64)
             {
                 valueInt64 = (long)uint64;
                 return true;
             }
-            else if( value is float ieee754 && System.Math.Floor(ieee754) == ieee754 )
+            else if (value is float ieee754 && System.Math.Floor(ieee754) == ieee754)
             {
                 valueInt64 = (long)ieee754;
                 return true;
             }
-            else if( value is double dieee754 && System.Math.Floor(dieee754) == dieee754 )
+            else if (value is double dieee754 && System.Math.Floor(dieee754) == dieee754)
             {
                 valueInt64 = (long)dieee754;
                 return true;
