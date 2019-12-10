RicoSuter/NJsonSchema

commit 543724234ea7fc97e1d81ce7cc9fd2ca76c32fa8

Dictionary key as object for TS v2.1 and higher

src/NJsonSchema.CodeGeneration.TypeScript/TypeScriptTypeResolver.cs
@@ -96,11 +96,20 @@ namespace NJsonSchema.CodeGeneration.TypeScript
 
             if (schema.IsDictionary)
             {
+                var defaultType = "string";
+
                 var prefix = addInterfacePrefix &&
                     SupportsConstructorConversion(schema.AdditionalPropertiesSchema) &&
                     schema.AdditionalPropertiesSchema?.ActualSchema.Type.HasFlag(JsonObjectType.Object) == true ? "I" : "";
+
                 var valueType = prefix + ResolveDictionaryValueType(schema, "any", Settings.SchemaType);
-                return $"{{ [key: string] : {valueType}; }}";
+                var keyType = this.Settings.TypeScriptVersion >= 2.1m ? prefix + ResolveDictionaryKeyType(schema, defaultType, Settings.SchemaType): defaultType;
+
+                if (keyType == defaultType) {
+                    return $"{{ [key: {keyType}] : {valueType}; }}";
+                }
+
+                return $"{{ [key in keyof typeof {keyType}] : {valueType}; }}";
             }
 
             return (addInterfacePrefix && SupportsConstructorConversion(schema) ? "I" : "") +

src/NJsonSchema.CodeGeneration.TypeScript.Tests/TypeScriptDictionaryTests.cs
@@ -38,6 +38,22 @@ namespace NJsonSchema.CodeGeneration.TypeScript.Tests
             Assert.Contains("Mapping2: { [key: string] : string; };", code);
         }
 
+        [Fact]
+        public async Task When_dictionary_key_is_enum_then_typescript_has_enum_key_ts_2_1()
+        {
+            //// Arrange
+            var schema = await JsonSchema4.FromTypeAsync<EnumKeyDictionaryTest>();
+            var data = schema.ToJson();
+
+            //// Act
+            var generator = new TypeScriptGenerator(schema, new TypeScriptGeneratorSettings { TypeStyle = TypeScriptTypeStyle.Interface, TypeScriptVersion = 2.1m });
+            var code = generator.GenerateFile("MyClass");
+
+            //// Assert
+            Assert.Contains("Mapping: { [key in keyof typeof PropertyName] : string; } | undefined;", code);
+            Assert.Contains("Mapping2: { [key in keyof typeof PropertyName] : string; } | undefined;", code);
+        }
+
         [JsonConverter(typeof(StringEnumConverter))]
         public enum Gender
         {

src/NJsonSchema.CodeGeneration/TypeResolverBase.cs
@@ -102,5 +102,18 @@ namespace NJsonSchema.CodeGeneration
 
             return fallbackType;
         }
+
+        /// <summary>Resolves the type of the dictionary key of the given schema (must be a dictionary schema).</summary>
+        /// <param name="schema">The schema.</param>
+        /// <param name="fallbackType">The fallback type (e.g. 'object').</param>
+        /// <param name="schemaType">The schema type.</param>
+        /// <returns>The type.</returns>
+        protected string ResolveDictionaryKeyType(JsonSchema4 schema, string fallbackType, SchemaType schemaType)
+        {
+            if (schema.Item != null)
+                return Resolve(schema.Item, schema.Item.ActualSchema.IsNullable(schemaType), null);
+
+            return fallbackType;
+        }
     }
 }

src/NJsonSchema/Generation/JsonSchemaGenerator.cs
@@ -465,7 +465,24 @@ namespace NJsonSchema.Generation
         {
             var genericTypeArguments = type.GetGenericTypeArguments();
 
+            var keyType = genericTypeArguments.Length == 2 ? genericTypeArguments[0] : typeof(string);
+
+            if (keyType != typeof(string)) {
+                var itemSchema = await GenerateAsync(keyType, schemaResolver).ConfigureAwait(false);
+                var valueTypeDescription = Settings.ReflectionService.GetDescription(keyType, null, Settings);
+                if (valueTypeDescription.RequiresSchemaReference(Settings.TypeMappers))
+                {
+                    schema.Item = new JsonSchema4
+                    {
+                        Reference = itemSchema
+                    };
+                }
+                else
+                    schema.Item = itemSchema;
+            }
+
             var valueType = genericTypeArguments.Length == 2 ? genericTypeArguments[1] : typeof(object);
+
             if (valueType == typeof(object))
                 schema.AdditionalPropertiesSchema = JsonSchema4.CreateAnySchema();
             else

commit 0f12b460a0f16c7cc3434b62a1fc0e17f2e199bc

Fix TypeScript generator generating invalid keys
When one has a dictionary that has keys of non-string type that are
also not enums, they should be generated as string keys on the
TypeScript side.

The correct type is resolved (`string`) but an interface prefix is added
for some reason, which breaks the generated TypeScript since there is
no type called `Istring`.

This bug seems to have been introduced with commit
543724234ea7fc97e1d81ce7cc9fd2ca76c32fa8
from pull request #755.

This commit should fix issue #898.

src/NJsonSchema.CodeGeneration.TypeScript/TypeScriptTypeResolver.cs
@@ -145,13 +145,20 @@ namespace NJsonSchema.CodeGeneration.TypeScript
                 var valueType = prefix + ResolveDictionaryValueType(schema, "any");
 
                 var defaultType = "string";
-                var keyType = Settings.TypeScriptVersion >= 2.1m ? prefix + ResolveDictionaryKeyType(schema, defaultType) : defaultType;
-                if (keyType != defaultType)
+                var resolvedType = ResolveDictionaryKeyType(schema, defaultType);
+
+                if (resolvedType != defaultType)
                 {
-                    return $"{{ [key in keyof typeof {keyType}]: {valueType}; }}";
+                    var keyType = Settings.TypeScriptVersion >= 2.1m ? prefix + resolvedType : defaultType;
+                    if (keyType != defaultType)
+                    {
+                        return $"{{ [key in keyof typeof {keyType}]: {valueType}; }}";
+                    }
+
+                    return $"{{ [key: {keyType}]: {valueType}; }}";
                 }
 
-                return $"{{ [key: {keyType}]: {valueType}; }}";
+                return $"{{ [key: {resolvedType}]: {valueType}; }}";
             }
 
             return (addInterfacePrefix && !schema.ActualTypeSchema.IsEnumeration && SupportsConstructorConversion(schema) ? "I" : "") +

src/NJsonSchema.CodeGeneration.TypeScript.Tests/DictionaryTests.cs
@@ -259,5 +259,50 @@ namespace NJsonSchema.CodeGeneration.TypeScript.Tests
                 }
             }
         }
+
+        [Fact]
+        public async Task When_dictionary_has_arbitrary_nonenum_key_then_generated_typescript_uses_plain_string_key()
+        {
+            //// Arrange
+            var json = @"{
+    ""properties"": {
+        ""myDict"": {
+            ""type"": ""object"",
+            ""additionalProperties"": {
+                ""$ref"": ""#/definitions/myItem""
+            }
+        }
+    },
+    ""definitions"": {
+        ""myItem"": {
+            ""type"": ""object"",
+            ""additionalProperties"": false,
+            ""properties"": {
+                ""name"": {
+                    ""type"": ""string""
+                },
+                ""age"": {
+                    ""type"": ""integer""
+                }
+            }
+        }
+    }
+}";
+
+            var schema = await JsonSchema.FromJsonAsync(json);
+
+            //// Act
+            var codeGenerator = new TypeScriptGenerator(schema, new TypeScriptGeneratorSettings
+            {
+                ConvertConstructorInterfaceData = true,
+                TypeStyle = TypeScriptTypeStyle.Class,
+                TypeScriptVersion = 2.7m
+            });
+
+            var code = codeGenerator.GenerateFile("Test");
+
+            //// Assert
+            Assert.DoesNotContain("[key in keyof typeof Istring]", code);
+        }
     }
 }