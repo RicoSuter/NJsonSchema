beccaacb1f1ca32d36ab6f041fba13dca41b3337
Sampo Kivistö <sampo.kivisto@visma.com>
2020-01-10 13:03:53 +0200

    Removed old test case

--- src/NJsonSchema.Tests/Generation/ExtensionDataGenerationTests.cs
+++ src/NJsonSchema.Tests/Generation/ExtensionDataGenerationTests.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
-using NJsonSchema.Annotations;
 using NJsonSchema.Generation;
 using Xunit;
 
@@ -28,42 +27,6 @@ namespace NJsonSchema.Tests.Generation
             Assert.Equal(1, schema.ActualProperties.Count);
             Assert.True(schema.AllowAdditionalProperties);
             Assert.True(schema.AdditionalPropertiesSchema.ActualSchema.IsAnyType);
-        }
-
-        public class SubData2
-        {
-            public int id { get; set; }
-        }
-
-        public class ClassWithExtensionData2
-        {
-            [JsonSchemaExtensionDataAttribute("x-other", "barfoo")]
-            public string other { get; set; }
-
-            [JsonSchemaExtensionDataAttribute("x-data", "foobar")]
-            public SubData2 data { get; set; }
-        }
-
-        public class ClassWithoutExtensionData2
-        {
-            public string other { get; set; }
-
-            public SubData2 noAttribute { get; set; }
-        }
-
-        public class Wrapper2
-        {
-            public ClassWithoutExtensionData2 noExtensionData { get; set; }
-
-            public ClassWithExtensionData2 hasExtensionData { get; set; }
-        }
-
-        [Fact]
-        public async Task When_class_has_property_with_JsonExtensionDataAttribute_on_property_then_AdditionalProperties_schema_is_set_with_values()
-        {
-            //// Act
-            var schema = JsonSchema.FromType<Wrapper2>(new JsonSchemaGeneratorSettings { SchemaType = SchemaType.OpenApi3 });
-            var json = schema.ToJson();
         }
     }
 }
