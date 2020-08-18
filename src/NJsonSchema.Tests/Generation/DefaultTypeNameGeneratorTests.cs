428b300f15320950a1c602ba5dea23d203214f30
Archigo <aspo-ja@hotmail.com>
2020-08-18 22:41:12 +0200

    DefaultTypeNameGenerator remove illegal characters DefaultTypeNameGenerator test removal of illegal characters

--- src/NJsonSchema.Tests/Generation/DefaultTypeNameGeneratorTests.cs
+++ src/NJsonSchema.Tests/Generation/DefaultTypeNameGeneratorTests.cs
@@ -32,5 +32,34 @@ namespace NJsonSchema.Tests.Generation
                 Assert.Equal(p.Value, name);
             }
         }
+
+        [Fact]
+        public void When_type_name_is_genererated_then_it_removes_Illegal_characters()
+        {
+            var tests = new Dictionary<string, string>
+            {
+                { "foo.bar+", "Bar" },
+                { "foo+Bar", "Foo_Bar" },
+                { "foo++Bar", "Foo_Bar" },
+                { "abc.1fooBar", "_1fooBar" },
+                { "foo9[bar]", "Foo9OfBar" },
+                { "bar[a+,b]", "BarOfA_AndB" },
+                { "foo.bar[a.b+]", "BarOfB" },
+                { "foo.bar[a.b+, c.+d+]", "BarOfB_And_d" },
+                { " Foo.ActionRes+ponse [System.Net.HttpSta+tusCode, Us+er]", "ActionRes_ponseOfHttpSta_tusCodeAndUs_er" }
+            };
+
+            //// Arrange
+            var generator = new DefaultTypeNameGenerator();
+
+            foreach (var p in tests)
+            {
+                //// Act
+                var name = generator.Generate(new JsonSchema(), p.Key, new List<string>());
+
+                //// Assert
+                Assert.Equal(p.Value, name);
+            }
+        }
     }
 }
