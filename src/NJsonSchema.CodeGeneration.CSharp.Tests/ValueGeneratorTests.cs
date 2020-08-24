f71e1decd6a9f01d07727e30c8b6c20c8a6e50bc
Lauris Jansons <lauris.jansons@visma.com>
2020-08-24 15:03:08 +0300

    #915: Assign default value to DateTime and DateTime? types as parsed string literal

--- src/NJsonSchema.CodeGeneration.CSharp.Tests/ValueGeneratorTests.cs
+++ src/NJsonSchema.CodeGeneration.CSharp.Tests/ValueGeneratorTests.cs
@@ -64,5 +64,34 @@ namespace NJsonSchema.CodeGeneration.Tests.CSharp
             /// Assert
             Assert.Contains("public int? PageSize { get; set; } = 10;", code);
         }
+
+        [Fact]
+        public async Task When_property_is_string_and_format_is_date_time_then_assign_default_value()
+        {
+            /// Arrange
+            var json = @"{
+                ""type"": ""object"",
+                ""properties"": {
+	                ""dateTime"": {
+		                ""type"": ""string"",
+		                ""format"": ""date-time"",
+		                ""default"": ""31.12.9999 23:59:59""
+	                }
+                }
+            }";
+            var schema = await JsonSchema.FromJsonAsync(json);
+
+            /// Act
+            var generator = new CSharpGenerator(schema, new CSharpGeneratorSettings
+            {
+                ClassStyle = CSharpClassStyle.Poco,
+                SchemaType = SchemaType.Swagger2,
+                DateTimeType = "System.DateTime"
+            });
+            var code = generator.GenerateFile("MyClass");
+
+            /// Assert
+            Assert.Contains("public System.DateTime? DateTime { get; set; } = System.DateTime.Parse(\"31.12.9999 23:59:59\");", code);
+        }
     }
 }
