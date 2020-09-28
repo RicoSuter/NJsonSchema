70d38730521566358411063611a82b7509297ccc
Sampo Kivistö <sampo.kivisto@visma.com>
2020-09-28 15:02:20 +0300

    Adds support for custom schema extension data attributes (#1254)
    * Adds support for custom schema extension data attributes
    
    * Refactor
    
    Co-authored-by: Rico Suter <mail@rsuter.com>

---
+++ src/NJsonSchema/Annotations/IJsonSchemaExtensionDataAttribute.cs
@@ -0,0 +1,22 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="JsonSchemaAttribute.cs" company="NJsonSchema">
+//     Copyright (c) Rico Suter. All rights reserved.
+// </copyright>
+// <license>https://github.com/RicoSuter/NJsonSchema/blob/master/LICENSE.md</license>
+// <author>Rico Suter, mail@rsuter.com</author>
+//-----------------------------------------------------------------------
+
+using System;
+
+namespace NJsonSchema.Annotations
+{
+    /// <summary>Interface to add an extension data property to a class or property, implementation needs to inherit from System.Attribute.</summary>
+    public interface IJsonSchemaExtensionDataAttribute
+    {
+        /// <summary>Gets the property name.</summary>
+        string Key { get; }
+
+        /// <summary>Gets the value.</summary>
+        object Value { get; }
+    }
+}
