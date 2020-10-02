7a3a7117ede91440ca87382a3cbf792dcbe16a95
Alexander WB <alexander.bladh@gmail.com>
2020-10-02 17:54:35 +0200

    Support Array default (#1262)
    When using ToSampleJson() the arrays will be populated with
    the default value of the item. Now it's populated by the default value
    if it exists.

--- src/NJsonSchema.Tests/Generation/SampleJsonDataGeneratorTests.cs
+++ src/NJsonSchema.Tests/Generation/SampleJsonDataGeneratorTests.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json.Linq;
 using NJsonSchema.Generation;
+using System.ComponentModel;
 using Xunit;
 
 namespace NJsonSchema.Tests.Generation
@@ -26,6 +27,12 @@ namespace NJsonSchema.Tests.Generation
         public class Student : Person
         {
             public string Course { get; set; }
+        }
+
+        public class Measurements
+        {
+            [DefaultValue(new int[] {1,2,3})]
+            public int[] Weights;
         }
 
         [Fact]
@@ -64,5 +71,21 @@ namespace NJsonSchema.Tests.Generation
             Assert.NotNull(obj.Property(nameof(Person.MainAddress)));
             Assert.NotNull(obj.Property(nameof(Person.Addresses)));
         }
+
+        [Fact]
+        public void Default_values_are_set_for_arrays()
+        {
+            //// Arrange
+            var schema = JsonSchema.FromType<Measurements>();
+            var generator = new SampleJsonDataGenerator();
+
+            //// Act
+            var token = generator.Generate(schema);
+            var obj = token as JObject;
+
+            //// Assert
+            Assert.Equal(new JArray(new int[] { 1, 2, 3 }), obj.GetValue(nameof(Measurements.Weights)));
+        }
+
     }
 }
