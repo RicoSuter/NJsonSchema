d53a7d49074d4b9f770f3f921af564e12c0452d7
Christian Wengel <christian.wengel@gmail.com>
2021-02-08 17:51:38 +0100

    Add System.Text.Json extension attribute tests

---
+++ src/NJsonSchema.Tests/Generation/SystemTextJson/SystemTextJsonExtensionDataGenerationTests.cs
@@ -0,0 +1,34 @@
+﻿#if !NET46 && !NET452
+
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using NJsonSchema.Generation;
+using Xunit;
+
+namespace NJsonSchema.Tests.Generation.SystemTextJson
+{
+    public class SystemTextJsonExtensionDataGenerationTests
+    {
+        public class ClassWithExtensionData
+        {
+            public string Foo { get; set; }
+
+            [JsonExtensionData]
+            public IDictionary<string, object> ExtensionData { get; set; }
+        }
+
+        [Fact]
+        public void SystemTextJson_When_class_has_property_with_JsonExtensionDataAttribute_on_property_then_AdditionalProperties_schema_is_set()
+        {
+            //// Act
+            var schema = JsonSchema.FromType<ClassWithExtensionData>(new JsonSchemaGeneratorSettings { SchemaType = SchemaType.OpenApi3 });
+
+            //// Assert
+            Assert.Equal(1, schema.ActualProperties.Count);
+            Assert.True(schema.AllowAdditionalProperties);
+            Assert.True(schema.AdditionalPropertiesSchema.ActualSchema.IsAnyType);
+        }
+    }
+}
+
+#endif
