5ad7b5e5bc2d6e31058b73af501d9a93d3e7d3ed
xueling <yuxxxling@qq.com>
2021-03-04 18:03:17 +0800

    When class with [DataContract], field or property with [DataMember] is not ignored.

--- src/NJsonSchema.Tests/Generation/SchemaGenerationTests.cs
+++ src/NJsonSchema.Tests/Generation/SchemaGenerationTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -169,6 +171,82 @@ namespace NJsonSchema.Tests.Generation
             // Assert
             Assert.Equal(1, schema.ActualProperties.Count);
             Assert.True(schema.ActualProperties.ContainsKey("Bar"));
+        }
+        
+        
+
+        [DataContract]
+        class ClassWithPrivateDataMember1
+        {
+            [IgnoreDataMember]
+            public string MyField
+            {
+                get => _myField;
+                set
+                {
+                    // Do some stuff, except for deserializing.
+                    _myField = value;
+                }
+            }
+
+            [DataMember(Name = nameof(MyField))] 
+            [Display(Name = "My Field", Description = "......")]
+            private string _myField;
+        }
+
+        [Fact]
+        public async Task When_private_field_is_dataMember_then_it_is_not_ignored1()
+        {
+            // Act
+            var schema = JsonSchema.FromType<ClassWithPrivateDataMember1>();
+            var json = schema.ToJson();
+
+            // Assert
+            Assert.Equal(1, schema.ActualProperties.Count);
+            Assert.True(schema.ActualProperties.ContainsKey("MyField"));
+        }
+        
+        [DataContract]
+        class ClassWithPrivateDataMember2
+        {
+            [DataMember(Name = nameof(MyField))] 
+            [Display(Name = "My Field", Description = "......")]
+            private int _myField;
+            
+            [IgnoreDataMember]
+            public string MyField { get; set; }
+        }
+
+        [Fact]
+        public async Task When_private_field_is_dataMember_then_it_is_not_ignored2()
+        {
+            // Act
+            var schema = JsonSchema.FromType<ClassWithPrivateDataMember2>();
+            var json = schema.ToJson();
+
+            // Assert
+            Assert.Equal(1, schema.ActualProperties.Count);
+            Assert.True(schema.ActualProperties.ContainsKey("MyField"));
+            Assert.Equal(JsonObjectType.Integer, schema.Properties["MyField"].Type);
+        }
+        
+        [DataContract]
+        class ClassWithPrivateDataMember3
+        {
+            [DataMember]
+            private string MyField { get; set; }
+        }
+
+        [Fact]
+        public async Task When_private_property_is_dataMember_then_it_is_not_ignored()
+        {
+            // Act
+            var schema = JsonSchema.FromType<ClassWithPrivateDataMember3>();
+            var json = schema.ToJson();
+
+            // Assert
+            Assert.Equal(1, schema.ActualProperties.Count);
+            Assert.True(schema.ActualProperties.ContainsKey("MyField"));
         }
 
         // Used as demo for https://github.com/swagger-api/swagger-ui/issues/1056
