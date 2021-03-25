6f6bdd2ce63f5d05103a0687c9b54b6c530809fc
Marko Lahma <marko.lahma@gmail.com>
2021-03-25 13:25:09 +0200

    Create benchmarks

---
+++ src/NJsonSchema.Benchmark/JsonSchemaBenchmark.cs
@@ -0,0 +1,32 @@
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+
+namespace NJsonSchema.Benchmark
+{
+    [MemoryDiagnoser]
+    public class JsonSchemaBenchmark
+    {
+        private string _json;
+        
+        [GlobalSetup]
+        public async Task Setup()
+        {
+            _json = await ReadJson();
+        }
+
+        internal static Task<string> ReadJson()
+        {
+            var assembly = typeof(JsonSchemaBenchmark).Assembly;
+            var file = assembly.GetManifestResourceNames().First(x => x.Contains("LargeSchema.json"));
+            return new StreamReader(assembly.GetManifestResourceStream(file)).ReadToEndAsync();
+        }
+
+        [Benchmark]
+        public Task<JsonSchema> FromJsonAsync()
+        {
+            return JsonSchema.FromJsonAsync(_json);
+        }
+    }
+}
