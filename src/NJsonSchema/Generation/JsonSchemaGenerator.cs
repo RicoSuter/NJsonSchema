4d534e403cae78af98f821abc814ecebc034e966
Christian Wengel <christian.wengel@gmail.com>
2021-05-08 13:21:24 +0200

    Fix: System.Text.Json ExtensionData attribute allows object & JsonElement values

--- src/NJsonSchema/Generation/JsonSchemaGenerator.cs
+++ src/NJsonSchema/Generation/JsonSchemaGenerator.cs
@@ -767,6 +767,8 @@ namespace NJsonSchema.Generation
             {
                 var genericTypeArguments = extensionDataProperty.GenericArguments;
                 var extensionDataPropertyType = genericTypeArguments.Length == 2 ? genericTypeArguments[1] : typeof(object).ToContextualType();
+                if (extensionDataPropertyType.TypeInfo.FullName.Equals("System.Text.Json.JsonElement"))
+                    extensionDataPropertyType = typeof(object).ToContextualType();
 
                 schema.AdditionalPropertiesSchema = GenerateWithReferenceAndNullability<JsonSchema>(
                     extensionDataPropertyType, schemaResolver);
