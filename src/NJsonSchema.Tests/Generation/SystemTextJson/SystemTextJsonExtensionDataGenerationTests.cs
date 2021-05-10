6b66d5072bca00db56ec98733771dae87c9ef250
deceptiveSimplicity <christian.wengel@gmail.com>
2021-05-10 11:04:51 +0200

    System.Text.Json.Serialization.JsonExtensionDataAttribute allows object & JsonElement values (#1367)
    * Extend System.Text.Json ExtensionData attribute tests
    
    * Fix: System.Text.Json ExtensionData attribute allows object & JsonElement values
    
    * Revert "Fix: System.Text.Json ExtensionData attribute allows object & JsonElement values"
    
    This reverts commit 4d534e40
    
    * Fix: System.Text.Json ExtensionData attribute allows object & JsonElement values

--- src/NJsonSchema.Tests/Generation/SystemTextJson/SystemTextJsonExtensionDataGenerationTests.cs
+++ src/NJsonSchema.Tests/Generation/SystemTextJson/SystemTextJsonExtensionDataGenerationTests.cs
@@ -1,6 +1,7 @@
 ﻿#if !NET46 && !NET452
 
 using System.Collections.Generic;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 using NJsonSchema.Generation;
 using Xunit;
@@ -9,22 +10,46 @@ namespace NJsonSchema.Tests.Generation.SystemTextJson
 {
     public class SystemTextJsonExtensionDataGenerationTests
     {
-        public class ClassWithExtensionData
+        public class ClassWithObjectExtensionData
         {
             public string Foo { get; set; }
 
             [JsonExtensionData]
             public IDictionary<string, object> ExtensionData { get; set; }
         }
+        
+        public class ClassWithJsonElementExtensionData
+        {
+            public string Foo { get; set; }
+
+            [JsonExtensionData]
+            public IDictionary<string, JsonElement> ExtensionData { get; set; }
+        }
 
         [Fact]
-        public void SystemTextJson_When_class_has_property_with_JsonExtensionDataAttribute_on_property_then_AdditionalProperties_schema_is_set()
+        public void SystemTextJson_When_class_has_object_Dictionary_with_JsonExtensionDataAttribute_on_property_then_AdditionalProperties_schema_is_set()
         {
             //// Act
-            var schema = JsonSchema.FromType<ClassWithExtensionData>(new JsonSchemaGeneratorSettings
+            var schema = JsonSchema.FromType<ClassWithObjectExtensionData>(new JsonSchemaGeneratorSettings
             {
                 SchemaType = SchemaType.OpenApi3,
-                SerializerOptions = new System.Text.Json.JsonSerializerOptions()
+                SerializerOptions = new JsonSerializerOptions()
+            });
+
+            //// Assert
+            Assert.Equal(1, schema.ActualProperties.Count);
+            Assert.True(schema.AllowAdditionalProperties);
+            Assert.True(schema.AdditionalPropertiesSchema.ActualSchema.IsAnyType);
+        }
+        
+        [Fact]
+        public void SystemTextJson_When_class_has_JsonElement_Dictionary_with_JsonExtensionDataAttribute_on_property_then_AdditionalProperties_schema_is_set()
+        {
+            //// Act
+            var schema = JsonSchema.FromType<ClassWithJsonElementExtensionData>(new JsonSchemaGeneratorSettings
+            {
+                SchemaType = SchemaType.OpenApi3,
+                SerializerOptions = new JsonSerializerOptions()
             });
 
             //// Assert
