82941d0e16f54cc4ff70340c65f904fab1d7a00f
Guanbo Wang <GWang26@slb.com>
2021-06-16 14:04:57 +0800

    Fix XML comments on CodeGeneratorSettings

--- src/NJsonSchema.CodeGeneration.CSharp/CSharpGeneratorSettings.cs
+++ src/NJsonSchema.CodeGeneration.CSharp/CSharpGeneratorSettings.cs
@@ -124,7 +124,7 @@ namespace NJsonSchema.CodeGeneration.CSharp
         /// <summary>Gets or sets the name of a static method which is called to transform the JsonSerializerSettings (for Newtonsoft.Json) or the JsonSerializerOptions (for System.Text.Json) used in the generated ToJson()/FromJson() methods (default: null).</summary>
         public string JsonSerializerSettingsTransformationMethod { get; set; }
 
-        /// <summary>Gets or sets a value indicating whether to render ToJson() and FromJson() methods (default: true).</summary>
+        /// <summary>Gets or sets a value indicating whether to render ToJson() and FromJson() methods (default: false).</summary>
         public bool GenerateJsonMethods { get; set; }
 
         /// <summary>Gets or sets a value indicating whether enums should be always generated as bit flags (default: false).</summary>
