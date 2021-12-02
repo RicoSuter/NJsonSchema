3176d2402c00e2fab8bcee219e18e30f5e257ba0
Rico Suter <mail@rsuter.com>
2021-12-02 20:22:29 +0100

    Fix build on tag

--- build/Build.Publish.cs
+++ build/Build.Publish.cs
@@ -16,11 +16,11 @@ public partial class Build
     string MyGetGetSource => "https://www.myget.org/F/njsonschema/api/v2/package";
     [Parameter] [Secret] string MyGetApiKey;
 
-    string ApiKeyToUse => !string.IsNullOrWhiteSpace(TagVersion) ? NuGetApiKey : MyGetApiKey;
-    string SourceToUse => !string.IsNullOrWhiteSpace(TagVersion) ? NuGetSource : MyGetGetSource;
+    string ApiKeyToUse => IsTaggedBuild ? NuGetApiKey : MyGetApiKey;
+    string SourceToUse => IsTaggedBuild ? NuGetSource : MyGetGetSource;
 
     Target Publish => _ => _
-        .OnlyWhenDynamic(() => IsRunningOnWindows && (GitRepository.IsOnMainOrMasterBranch()))
+        .OnlyWhenDynamic(() => IsRunningOnWindows && (GitRepository.IsOnMainOrMasterBranch() || IsTaggedBuild))
         .DependsOn(Pack)
         .Requires(() => NuGetApiKey, () => MyGetApiKey)
         .Executes(() =>
