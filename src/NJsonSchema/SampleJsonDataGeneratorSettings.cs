a478fafb12bc9a7c243a9b58e8c595b0a0f0e661
Rico Suter <mail@rsuter.com>
2021-12-03 10:21:19 +0100

    Fixes

---
+++ src/NJsonSchema/SampleJsonDataGeneratorSettings.cs
@@ -0,0 +1,17 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="SampleJsonDataGeneratorSettings.cs" company="NJsonSchema">
+//     Copyright (c) Rico Suter. All rights reserved.
+// </copyright>
+// <license>https://github.com/RicoSuter/NJsonSchema/blob/master/LICENSE.md</license>
+// <author>Rico Suter, mail@rsuter.com</author>
+//-----------------------------------------------------------------------
+
+namespace NJsonSchema
+{
+    /// <summary> Settings for generating sample json data.</summary>
+    public class SampleJsonDataGeneratorSettings
+    {
+        /// <summary>Gets or sets a value indicating whether to generate optional properties (default: true).</summary>
+        public bool GenerateOptionalProperties { get; set; } = true;
+    }
+}
