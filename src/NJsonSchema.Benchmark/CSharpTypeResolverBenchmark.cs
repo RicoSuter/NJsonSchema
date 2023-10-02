d6f0d014ec48a429b40fa744b4fb1792056eed55
Marko Lahma <marko.lahma@gmail.com>
2023-10-02 17:55:39 +0300

    Optimize type resolving (#1629)
    * reuse same HashSet for reservedTypeNames instead re-creating on every cache miss
    * cleanup csprojs
    * remove build configurations for _build project in solution file
    * use string interpolation for better optimizations

---
+++ src/NJsonSchema.Benchmark/CSharpTypeResolverBenchmark.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using NJsonSchema.CodeGeneration.CSharp;
+
+namespace NJsonSchema.Benchmark;
+
+[MemoryDiagnoser]
+public class CSharpTypeResolverBenchmark
+{
+    private Dictionary<string, JsonSchema> _definitions;
+    private CSharpGeneratorSettings _settings;
+
+    [GlobalSetup]
+    public async Task Setup()
+    {
+        var json = await JsonSchemaBenchmark.ReadJson();
+        var schema = await JsonSchema.FromJsonAsync(json);
+        _definitions = schema.Definitions.ToDictionary(p => p.Key, p => p.Value);
+        _settings = new CSharpGeneratorSettings();
+    }
+
+    [Benchmark]
+    public void RegisterSchemaDefinitions()
+    {
+        var resolver = new CSharpTypeResolver(_settings, exceptionSchema: null);
+        resolver.RegisterSchemaDefinitions(_definitions);
+    }
+}
