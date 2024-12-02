b1f850d38a32b0efaef526091cc21d3eb02750b2
desjoerd <sjoerd.meer@outlook.com>
2024-12-02 17:34:44 +0100

    Fix TypeMapper UseReference not used when mapping a GenericType

--- src/NJsonSchema.Tests/Generation/TypeMapperTests.cs
+++ src/NJsonSchema.Tests/Generation/TypeMapperTests.cs
@@ -98,11 +98,12 @@ namespace NJsonSchema.Tests.Generation
         {
             public Type MappedType => typeof(MyWrapper<>);
 
-            public bool UseReference => true;
+            public bool UseReference => false;
 
             public void GenerateSchema(JsonSchema schema, TypeMapperContext context)
             {
-                schema.Reference = context.JsonSchemaGenerator.Generate(context.Type.GenericTypeArguments[0], context.JsonSchemaResolver);
+                context.JsonSchemaGenerator.Generate(schema, context.Type.GenericTypeArguments[0], context.JsonSchemaResolver);
+                //schema.Reference = context.JsonSchemaGenerator.Generate(context.Type.GenericTypeArguments[0], context.JsonSchemaResolver);
             }
         }
 
@@ -122,6 +123,49 @@ namespace NJsonSchema.Tests.Generation
             var json = schema.ToJson();
             Assert.True(schema.Definitions.ContainsKey("MyBar"));
             Assert.False(schema.Definitions.ContainsKey("MyWrapperOfMyBar"));
+        }
+
+        public class MyStringFoo
+        {
+            public MyWrapper<string> PropertyString { get; set; }
+
+            public MyWrapper<MyBar> PropertyMyBar { get; set; }
+        }
+
+        public class WithoutReferenceTypeMapper : ITypeMapper
+        {
+            public Type MappedType => typeof(MyWrapper<>);
+
+            public bool UseReference => false;
+
+            public void GenerateSchema(JsonSchema schema, TypeMapperContext context)
+            {
+                context.JsonSchemaGenerator.Generate(schema, context.Type.GenericTypeArguments[0], context.JsonSchemaResolver);
+            }
+        }
+
+
+        [Fact]
+        public async Task When_generic_type_mapper_is_defined_then_it_is_called_and_the_use_reference_false_is_used()
+        {
+            // Act
+            var schema = NewtonsoftJsonSchemaGenerator.FromType<MyStringFoo>(new NewtonsoftJsonSchemaGeneratorSettings
+            {
+                TypeMappers =
+                {
+                    new WithoutReferenceTypeMapper()
+                }
+            });
+
+            // Assert
+            var json = schema.ToJson();
+            Assert.False(schema.Definitions.ContainsKey("string"));
+            Assert.False(schema.Definitions.ContainsKey("MyWrapperOfString"));
+            Assert.True(schema.Definitions.ContainsKey("MyBar"));
+
+            Assert.Contains("$ref", json); // The $ref to MyBar should be present
+            // There should only be one $ref, which is to Bar, this is the case when the first and last index of "$ref" is the same
+            Assert.True(json.IndexOf("$ref", StringComparison.Ordinal) == json.LastIndexOf("$ref", StringComparison.Ordinal));
         }
 
     }
