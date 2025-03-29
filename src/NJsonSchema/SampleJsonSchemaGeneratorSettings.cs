be0a93eeefa94ccd29a52aabec92eb6cdfc93545
javierlarota <javierlarota@gmail.com>
2025-03-29 09:01:53 -0400

    SampleJsonSchemaGenerator: Generate JSONSchema for openapi (#1767)
    * SampleJsonSchemaGenerator: Generate JSONSchema for openapi
    
    * Updating README.md
    
    * Fixing readme.md

---
+++ src/NJsonSchema/SampleJsonSchemaGeneratorSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace NJsonSchema
+{
+    /// <summary>
+    /// Settings for generating sample json schema
+    /// </summary>
+    public class SampleJsonSchemaGeneratorSettings
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether to generate optional properties (default: <see cref="SchemaType.JsonSchema"/>).
+        /// </summary>
+        public SchemaType SchemaType { get; set; } = SchemaType.JsonSchema;
+    }
+}
