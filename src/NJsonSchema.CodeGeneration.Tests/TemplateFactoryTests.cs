c86bbfc0060e45e7c16f1982715f208a2f64fe1e
Georgy Losenkov <georgy.losenkov@gmail.com>
2025-07-09 17:10:43 +0700

    Add ability to overwrite templates multiple times using templateFolder

--- src/NJsonSchema.CodeGeneration.Tests/TemplateFactoryTests.cs
+++ src/NJsonSchema.CodeGeneration.Tests/TemplateFactoryTests.cs
@@ -25,4 +25,35 @@ public class TemplateFactoryTests
 
         Assert.Equal("WELCOME TO THE LIQUID TAG", templateResult);
     }
+
+    [Theory]
+    [InlineData("Templates", "sample-1", "sample-1-in-templates")]
+    [InlineData("Templates", "sample-2", "sample-2-in-templates")]
+    [InlineData("Templates2", "sample-2", "sample-2-in-templates-2")]
+    [InlineData("Templates2", "sample-3", "sample-3-in-templates-2")]
+    [InlineData("Templates2;Templates", "sample-1", "sample-1-in-templates")]
+    [InlineData("Templates2;Templates", "sample-2", "sample-2-in-templates-2")]
+    [InlineData("Templates2;Templates", "sample-3", "sample-3-in-templates-2")]
+    [InlineData("Templates;Templates2", "sample-1", "sample-1-in-templates")]
+    [InlineData("Templates;Templates2", "sample-2", "sample-2-in-templates")]
+    [InlineData("Templates;Templates2", "sample-3", "sample-3-in-templates-2")]
+    public void Can_override_templates(string templateDirectory, string templateName, string expectedResult)
+    {
+        var generatorSettings = new CSharpGeneratorSettings
+        {
+            TemplateDirectory = templateDirectory,
+        };
+
+        var factory = new DefaultTemplateFactory(generatorSettings, [typeof(CSharpGeneratorSettings).Assembly])
+        {
+            FluidParser = new LiquidParser(new FluidParserOptions { AllowLiquidTag = true }),
+        };
+        generatorSettings.TemplateFactory = factory;
+
+        var template = factory.CreateTemplate("csharp", templateName, new { });
+
+        var templateResult = template.Render();
+
+        Assert.Equal(expectedResult, templateResult);
+    }
 }
