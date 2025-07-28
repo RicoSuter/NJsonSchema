e47a549aae7d7a25668f3b5a36275f57faaca9a6
Marko Lahma <marko.lahma@gmail.com>
2025-07-28 13:38:25 +0300

    Convert to xUnit v3

--- src/NJsonSchema.Benchmark/SchemaGenerationBenchmarks.cs
+++ src/NJsonSchema.Benchmark/SchemaGenerationBenchmarks.cs
@@ -1,32 +1,13 @@
 ﻿using System.Runtime.Serialization;
-using NBench;
 using NJsonSchema.NewtonsoftJson.Generation;
-using Pro.NBench.xUnit.XunitExtensions;
-using Counter = NBench.Counter;
 
 namespace NJsonSchema.Benchmark
 {
     public class SchemaGenerationBenchmarks
     {
-        private Counter _counter;
-
-        [PerfSetup]
-        public void Setup(BenchmarkContext context)
-        {
-            _counter = context.GetCounter("Iterations");
-        }
-
-        [NBenchFact(Skip = "Can vary a lot when running on GitHub Actions")]
-        [PerfBenchmark(
-            NumberOfIterations = 3,
-            RunTimeMilliseconds = 1000,
-            RunMode = RunMode.Throughput,
-            TestMode = TestMode.Test)]
-        [CounterThroughputAssertion("Iterations", MustBe.GreaterThan, 100)]
         public void GenerateSchema()
         {
             var schema = NewtonsoftJsonSchemaGenerator.FromType<Container>();
-            _counter.Increment();
         }
 
         public class SpecialTeacher : Teacher
--- src/NJsonSchema.Benchmark/SerializationBenchmarks.cs
+++ src/NJsonSchema.Benchmark/SerializationBenchmarks.cs
@@ -1,20 +1,13 @@
 ﻿using System.Reflection;
-using NBench;
-using Pro.NBench.xUnit.XunitExtensions;
-using Counter = NBench.Counter;
-
-[assembly: CollectionBehavior(DisableTestParallelization = true)]
 
 namespace NJsonSchema.Benchmark
 {
     public class SerializationBenchmarks
     {
         private JsonSchema _schema;
-        private Counter _counter;
         private string _json;
 
-        [PerfSetup]
-        public void Setup(BenchmarkContext context)
+        public void Setup()
         {
             var executingAssembly = Assembly.GetExecutingAssembly();
 
@@ -26,41 +19,22 @@ namespace NJsonSchema.Benchmark
             }
 
             _schema = JsonSchema.FromJsonAsync(_json).Result;
-            _counter = context.GetCounter("Iterations");
         }
 
         /// <summary>
         /// Ensure that we can serialise at least 200 times per second (5ms).
         /// </summary>
-        [NBenchFact]
-        [PerfBenchmark(
-            Description = "Ensure serialization doesn't take too long",
-            NumberOfIterations = 3,
-            RunTimeMilliseconds = 1000,
-            RunMode = RunMode.Throughput,
-            TestMode = TestMode.Test)]
-        [CounterThroughputAssertion("Iterations", MustBe.GreaterThan, 200)]
         public void ToJson()
         {
             _schema.ToJson();
-            _counter.Increment();
         }
 
         /// <summary>
         /// Ensure that we can deserialise at least 200 times per second (5ms).
         /// </summary>
-        [NBenchFact]
-        [PerfBenchmark(
-            Description = "Ensure deserialization doesn't take too long",
-            NumberOfIterations = 3,
-            RunTimeMilliseconds = 1000,
-            RunMode = RunMode.Throughput,
-            TestMode = TestMode.Test)]
-        [CounterThroughputAssertion("Iterations", MustBe.GreaterThan, 200)]
         public void FromJson()
         {
             JsonSchema.FromJsonAsync(_json).GetAwaiter().GetResult();
-            _counter.Increment();
         }
     }
 }
